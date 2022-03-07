2819decfd20cad555fb2c5a49e70887820da3c34
Vinod <vinodsinghrawat1905@gmail.com>
2022-03-07 18:55:01 +0530

    fixed bug-104

--- src/main/java/in/ashokit/PwdUtils.java
+++ src/main/java/in/ashokit/PwdUtils.java
@@ -4,6 +4,13 @@ public class PwdUtils {
 
 	public void encode() {
 		// logic to encode
+		int a = 0;
+		String name = "ashokit";
+		System.out.println("new lines added");
+		int b = 20;
+		int c = a + b;
+		//Bug Fix- 104 changes
+		System.out.println(c);
 	}
 	
 	public void decode() {
