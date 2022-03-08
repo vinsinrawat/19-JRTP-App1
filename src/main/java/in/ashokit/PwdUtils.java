2d23c161b03164c12a9f9d755987650d723d596d
Vinod <vinodsinghrawat1905@gmail.com>
2022-03-08 13:48:49 +0530

    develop branch commit

--- src/main/java/in/ashokit/PwdUtils.java
+++ src/main/java/in/ashokit/PwdUtils.java
@@ -3,10 +3,10 @@ package in.ashokit;
 public class PwdUtils {
 
 	public void encode(String pwd) {
-		System.out.println("Encoding Password...!");
+		System.out.println("Encoding Password...! develop branch");
 	}
 	
 	public void decode(String str) {
-		System.out.println("Decoding Password...!");
+		System.out.println("Decoding Password...! develop branch");
 	}
 }
