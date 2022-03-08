51d200a51981ff4d09b57d33973804d345f9c251
Vinod <vinodsinghrawat1905@gmail.com>
2022-03-08 13:19:51 +0530

    changed encode method

--- src/main/java/in/ashokit/PwdUtils.java
+++ src/main/java/in/ashokit/PwdUtils.java
@@ -2,11 +2,11 @@ package in.ashokit;
 
 public class PwdUtils {
 
-	public void encode() {
+	public void encode(String pwd) {
 		System.out.println("Encoding Password...!");
 	}
 	
-	public void decode() {
+	public void decode(String str) {
 		System.out.println("Decoding Password...!");
 	}
 }
