e7ee5e0b0f1a17de898306f9e97d4f0bc3705236
Vinod <vinodsinghrawat1905@gmail.com>
2022-03-08 13:44:39 +0530

    feature-branch commit

--- src/main/java/in/ashokit/PwdUtils.java
+++ src/main/java/in/ashokit/PwdUtils.java
@@ -3,10 +3,10 @@ package in.ashokit;
 public class PwdUtils {
 
 	public void encode(String pwd) {
-		System.out.println("Encoding Password...!");
+		System.out.println("Encoding Password...! feature-branch");
 	}
 	
 	public void decode(String str) {
-		System.out.println("Decoding Password...!");
+		System.out.println("Decoding Password...!  feature-branch");
 	}
 }
