f0bf3568478910aced7b32c5c85fdcb2c1568178
Vinod <vinodsinghrawat1905@gmail.com>
2022-03-08 13:03:28 +0530

    changed pwdutils class

--- src/main/java/in/ashokit/PwdUtils.java
+++ src/main/java/in/ashokit/PwdUtils.java
@@ -3,17 +3,10 @@ package in.ashokit;
 public class PwdUtils {
 
 	public void encode() {
-		// logic to encode
-		int a = 0;
-		String name = "ashokit";
-		System.out.println("new lines added");
-		int b = 20;
-		int c = a + b;
-		//Bug Fix- 104 changes
-		System.out.println(c);
+		System.out.println("Encoding Password...!");
 	}
 	
 	public void decode() {
-		// logic to decode
+		System.out.println("Decoding Password...!");
 	}
 }
