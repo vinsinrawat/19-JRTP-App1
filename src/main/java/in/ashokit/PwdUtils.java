5371d6c4e7185609abd0399e7ea2df1032bef4ae
Vinod <vinodsinghrawat1905@gmail.com>
2022-03-08 13:54:51 +0530

    qa branch commit

--- src/main/java/in/ashokit/PwdUtils.java
+++ src/main/java/in/ashokit/PwdUtils.java
@@ -3,10 +3,10 @@ package in.ashokit;
 public class PwdUtils {
 
 	public void encode(String pwd) {
-		System.out.println("Encoding Password...!");
+		System.out.println("Encoding Password...! qa branch");
 	}
 	
 	public void decode(String str) {
-		System.out.println("Decoding Password...!");
+		System.out.println("Decoding Password...! qa branch");
 	}
 }
