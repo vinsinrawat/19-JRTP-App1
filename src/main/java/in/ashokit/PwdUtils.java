d7c8b42f116b3df01c1db7e35532300fc0e903ca
Vinod <vinodsinghrawat1905@gmail.com>
2022-03-09 12:05:26 +0530

    modified encode and decode methods

--- src/main/java/in/ashokit/PwdUtils.java
+++ src/main/java/in/ashokit/PwdUtils.java
@@ -2,12 +2,12 @@ package in.ashokit;
 
 public class PwdUtils {
 
-	public void encode(String pwd) {
-		System.out.println("Encoding Password...! release branch");
+	public void encode(String plainText) {
+		System.out.println("Encoding Password...!");
 	}
 	
-	public void decode(String str) {
-		System.out.println("Decoding Password...! release branch");
+	public void decode(String encodedText) {
+		System.out.println("Decoding Password...!");
 	}
 	
 }
